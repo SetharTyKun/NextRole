09a3c0b0f3094788b7f34bdc16a7af3d4f488d22
sethartykun <sethartykun16@gmail.com>
2026-05-03 22:26:05 +0700

    fix deploy 2

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
-import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
+    viteStaticCopy({
+      targets: [
+        { src: 'components', dest: '.' },
+        { src: 'js', dest: '.' },
+        { src: 'asset', dest: '.' },
+        { src: 'pages', dest: '.' },
+      ]
+    })
   ],
   server: {
     allowedHosts: true
